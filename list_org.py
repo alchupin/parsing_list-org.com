ecd698e7612ece8d0f99bcd8c00c40025f415c8a
alexey chupin <alchupin@gmail.com>
2019-10-31 10:53:19 +0300

    input/output types for functions added cosmetic changes in code

--- list_org.py
+++ list_org.py
@@ -1,27 +1,28 @@
 import os
 import csv
+from typing import Optional
 
 import requests
 from bs4 import BeautifulSoup
 
 
-def _get_html(url):
+def _get_html(url: str) -> Optional[str]:
     """
     Takes url address and returns response's content in Unicode
     :param url: url address: string
-    :return: None
+    :return: string if url available or None otherwise
     """
     try:
         r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
         return r.text
     except:
-        None
+        return None
 
 
-def _write_csv(data):
+def _write_csv(data: dict) -> None:
     """
     Writes data from dictionary into the file list_org.csv
-    :param data: information about company
+    :param data: information about company, dictionary
     :return: None
     """
     if not os.path.isfile('./list_org.csv'):
@@ -50,14 +51,13 @@ def _write_csv(data):
         )
 
 
-def get_company_info(url):
-    company_name = ''
-    head = ''
-    registration_date = ''
-    status = ''
-    inn = ''
-    kpp = ''
-    ogrn = ''
+def get_company_info(url: str):
+    """
+    Takes url address and writes information about company into the file list_org.csv
+    :param url: string
+    :return: None
+    """
+    info_n_a = "Информация отсутствует"
 
     html_text = _get_html(url)
     soup = BeautifulSoup(html_text, 'lxml')
@@ -65,11 +65,11 @@ def get_company_info(url):
     try:
         company_name = soup.find_all('div', class_='c2m')[0].find('a').text
     except:
-        company_name = 'Информация отсутствует'
+        company_name = info_n_a
     try:
         ogrn = soup.find_all('div', class_='c2m')[2].find_all('p')[3].text.replace('ОГРН: ', '')
     except:
-        ogrn = 'Информация отсутствует'
+        ogrn = info_n_a
 
     try:
         table = soup.find('table', class_='tt')
@@ -87,30 +87,30 @@ def get_company_info(url):
                 try:
                     head = tds_all[-1].text
                 except:
-                    head = 'Информация отсутствует'
+                    head = info_n_a
             if 'ИНН' and 'КПП' in tds_all[0].text:
                 try:
                     inn = tds_all[-1].text.split(' ')[0]
                     kpp = tds_all[-1].text.split(' ')[-1]
                 except:
-                    inn = 'Информация отсутствует'
-                    kpp = 'Информация отсутствует'
+                    inn = info_n_a
+                    kpp = info_n_a
             if 'Дата регистрации' in tds_all[0].text:
                 try:
                     registration_date = tds_all[-1].text.split(' ')[0]
                 except:
-                    registration_date = 'Информация отсутствует'
+                    registration_date = info_n_a
             if 'Статус' in tds_all[0].text:
                 try:
                     status = tds_all[-1].text
                 except:
-                    status = 'Информация отсутствует'
+                    status = info_n_a
     else:
-        head = 'Информация отсутствует'
-        registration_date = 'Информация отсутствует'
-        status = 'Информация отсутствует'
-        inn = 'Информация отсутствует'
-        kpp = 'Информация отсутствует'
+        head = info_n_a
+        registration_date = info_n_a
+        status = info_n_a
+        inn = info_n_a
+        kpp = info_n_a
     data = {
         'company_name': company_name,
         'head': head,
